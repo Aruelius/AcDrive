03ace458e14fb7637979895e1f742712158a6f90
Aruelius.L <25380989+Aruelius@users.noreply.github.com>
2020-01-11 20:23:32 +0800

    Create __init__.py

---
+++ AcDrive/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+
+"""AcDrive 艾希云
+https://github.com/Aruelius/AcDrive"""
+
+__author__ = "Hsury"
+__email__ = "i@hsury.com"
+__license__ = "SATA"
+__version__ = "2020.01.11"
